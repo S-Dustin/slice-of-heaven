24593dc5500c46648797030cb785f08ed726132b
Dustin Sauter <169107693+S-Dustin@users.noreply.github.com>
2024-05-19 18:26:39 -0500

    Delete backend directory
    restructuring

--- backend/utils/jwt.js
+++
@@ -1,8 +0,0 @@
-// utils/jwt.js
-const jwt = require('jsonwebtoken');
-
-const generateToken = (user) => {
-    return jwt.sign({ id: user._id, username: user.username, role: user.role }, 'your_jwt_secret', { expiresIn: '1h' });
-};
-
-module.exports = { generateToken };
