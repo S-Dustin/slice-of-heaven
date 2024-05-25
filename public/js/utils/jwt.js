b457d3d98a75d4aaadeb1d26bd01cd94498ffbba
Dustin Sauter <169107693+S-Dustin@users.noreply.github.com>
2024-05-25 03:08:55 -0500

    Delete public/js/utils directory
    moving utils to src directory

--- public/js/utils/jwt.js
+++
@@ -1,8 +0,0 @@
-// utils/jwt.js
-const jwt = require('jsonwebtoken');
-
-const generateToken = (user) => {
-    return jwt.sign({ id: user._id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-};
-
-module.exports = { generateToken };
